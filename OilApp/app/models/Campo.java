4339a0d53c1bbd52c0828828aaf9966c901d500a
Andrés Sandoval <a.sandoval1303@uniandes.edu.co>
2016-08-30 16:39:15 -0500

    Campo

--- OilApp/app/models/Campo.java
+++ OilApp/app/models/Campo.java
@@ -12,4 +12,20 @@ public class Campo extends Model{
     @Id
     @GeneratedValue
     private Long id;
+
+    private String region;
+
+    private String tipoSensor;
+
+    public Campo(String regionN){
+        region = regionN;
+    }
+
+    public String getRegion(){
+        return region;
+    }
+
+    public void setRegion(String regionN){
+        region=regionN;
+    }
 }
