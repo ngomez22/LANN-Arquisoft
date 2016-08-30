57d13bd93c78eca1a1ba5165944946d499c8cd4c
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2016-08-29 23:05:37 -0500

    Progress in Usuario model

--- OilApp/app/models/Usuario.java
+++ OilApp/app/models/Usuario.java
@@ -3,11 +3,69 @@ package models;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "usuarios")
 public class Usuario {
+
+    //public static Finder<Long,Usuario> FINDER = new Finder<>(Usuario.class);
+
     @Id
-    @GeneratedValue
+    @GeneratedValue//(strategy= GenerationType.SEQUENCE,generator = "usuariosId")
     private Long id;
 
+    private String nombre;
+
+    private Integer nivelAcceso;
+
+    public Usuario() {
+        this.id = null;
+        this.nombre = "NO NAME";
+        this.nivelAcceso = -1;
+    }
+
+    public Usuario(Long id) {
+        this();
+        this.id = id;
+    }
+
+    public Usuario(Long id, String nombre, Integer nivelAcceso) {
+        this.id = id;
+        this.nombre = nombre;
+        this.nivelAcceso = nivelAcceso;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getNivelAcceso() {
+        return nivelAcceso;
+    }
+
+    public void setNivelAcceso(Integer nivelAcceso) {
+        this.nivelAcceso = nivelAcceso;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", nivelAcceso=" + nivelAcceso +
+                '}';
+    }
 }
