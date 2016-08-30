51e2d21add9d3997aaf8d35e53a3efadc17efde1
l.castro12 <l.castro12@uniandes.edu.co>
2016-08-29 23:40:12 -0500

    Sensor

--- OilApp/app/models/Sensor.java
+++ OilApp/app/models/Sensor.java
@@ -11,4 +11,17 @@ public class Sensor extends Model{
     @Id
     @GeneratedValue
     private Long id;
+    private String tipoSensor;
+
+    public Sensor(String tipo){
+        tipoSensor = tipo;
+    }
+
+    public String getTipo(){
+        return tipoSensor;
+    }
+
+    public void setTipo(String tipo){
+        tipoSensor=tipo;
+    }
 }
