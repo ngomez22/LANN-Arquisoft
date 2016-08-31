59008ab4427ebac5a55c1750e560067208026213
l.castro12 <l.castro12@uniandes.edu.co>
2016-08-31 17:37:23 -0500

    Fix problems

--- OilApp/app/dispatchers/AkkaDispatcher.java
+++ OilApp/app/dispatchers/AkkaDispatcher.java
@@ -1,12 +1,9 @@
 package dispatchers;
 
-
 import akka.dispatch.MessageDispatcher;
 import play.libs.Akka;
-
-/**
- * Nicolás Gómez
- */
 public class AkkaDispatcher {
+
     public static MessageDispatcher jdbcDispatcher =  Akka.system().dispatchers().lookup("contexts.jdbc-dispatcher");
+
 }
