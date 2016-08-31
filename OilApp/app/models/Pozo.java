40d192d99822dc03604ed3b7c62ac0d040b56751
Nicolas Vasquez <n.vasquez10@uniandes.edu.co>
2016-08-30 19:49:43 -0500

    Model Pozo

--- OilApp/app/models/Pozo.java
+++ OilApp/app/models/Pozo.java
@@ -3,13 +3,98 @@ package models;
 
 import com.avaje.ebean.Model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
+import models.*;
 @Entity
 public class Pozo extends Model{
     @Id
     @GeneratedValue
     private Long id;
+
+
+    @OneToOne
+    private Sensor sensorCaudal;
+
+    @OneToOne
+    private Sensor sensorEnergia;
+
+    @OneToOne
+    private Sensor sensorTemperatura;
+
+    @OneToOne
+    private Sensor sensorEmergencia;
+
+    private String estado;
+
+    @ManyToOne
+    private Campo campo;
+
+
+    public Pozo(Sensor sensorCaudal, Sensor sensorEnergia, Sensor sensorTemperatura, Sensor sensorEmergencia, String estado, Campo campo) {
+        this.sensorCaudal = sensorCaudal;
+        this.sensorEnergia = sensorEnergia;
+        this.sensorTemperatura = sensorTemperatura;
+        this.sensorEmergencia = sensorEmergencia;
+        this.estado = estado;
+        this.campo = campo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Sensor getSensorCaudal() {
+        return sensorCaudal;
+    }
+
+    public void setSensorCaudal(Sensor sensorCaudal) {
+        this.sensorCaudal = sensorCaudal;
+    }
+
+    public Sensor getSensorEnergia() {
+        return sensorEnergia;
+    }
+
+    public void setSensorEnergia(Sensor sensorEnergia) {
+        this.sensorEnergia = sensorEnergia;
+    }
+
+    public Sensor getSensorTemperatura() {
+        return sensorTemperatura;
+    }
+
+    public void setSensorTemperatura(Sensor sensorTemperatura) {
+        this.sensorTemperatura = sensorTemperatura;
+    }
+
+    public Sensor getSensorEmergencia() {
+        return sensorEmergencia;
+    }
+
+    public void setSensorEmergencia(Sensor sensorEmergencia) {
+        this.sensorEmergencia = sensorEmergencia;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Campo getCampo() {
+        return campo;
+    }
+
+    public void setCampo(Campo campo) {
+        this.campo = campo;
+    }
+
+
 }
