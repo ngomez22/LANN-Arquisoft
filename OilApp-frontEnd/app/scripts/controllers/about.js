5b8b30451ac271ee96e5035b4ff1e8c9e009f01e
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2016-10-13 21:53:50 -0500

    Added front end AngularJS project

---
+++ OilApp-frontEnd/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name oilAppFrontEndApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the oilAppFrontEndApp
+ */
+angular.module('oilAppFrontEndApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
