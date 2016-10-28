c81820924a1b9119c9161c183aeb69e32644c05f
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2016-10-27 21:59:23 -0500

    deleted residual js files

--- OilApp/public/modules/about/about.mod.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Nicolás on 25/10/16.
- */
-(function (ng){
-    var mod = ng.module("aboutModule",["ui.bootstrap"]);
-    mod.constant("aboutContext", "/about");
-})(window.angular);
